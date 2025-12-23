748cc7a641071001cd81ac5a14a439c6c46373f5
PahanSoloEasy <skokovvova2@gmail.com>
2025-12-23 19:05:33 +0300

    Нашёл тайлы для pacman и организовал файлы для марио и пакмена отдельно для понимания

--- For BackGrounds/OsnovaPacMan.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="OsnovaPacMan" tilewidth="8" tileheight="8" tilecount="840" columns="28">
- <image source="../../../Загрузки/pacman-sheet.png" width="224" height="240"/>
-</tileset>
