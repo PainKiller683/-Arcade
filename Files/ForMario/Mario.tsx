67954640065b2f8197676df01515b8ecbaca23f0
PahanSoloEasy <skokovvova2@gmail.com>
2025-12-23 19:06:45 +0300

    Переименовавал основную папку

---
+++ Files/ForMario/Mario.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Mario" tilewidth="16" tileheight="32" tilecount="504" columns="36">
+ <image source="NES - Super Mario Bros. - Playable Characters - Mario &amp; Luigi.png" width="584" height="469"/>
+</tileset>
